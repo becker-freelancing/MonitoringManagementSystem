5e757987ef69ae401eb70efc77ddaeeac13403b2
jasb <jason.becker@parcIT.de>
2024-03-28 11:18:37 +0100

    Basic File Structure

---
+++ backend/mms-starter/src/test/java/com/jabasoft/mms/MainTest.java
@@ -0,0 +1,14 @@
+package com.jabasoft.mms;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+	@Test
+	void starts(){
+		Main.main(new String[0]);
+	}
+
+}
