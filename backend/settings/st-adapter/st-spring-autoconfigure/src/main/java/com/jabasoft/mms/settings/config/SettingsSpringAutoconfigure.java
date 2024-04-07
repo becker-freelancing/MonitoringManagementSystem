3ff3d9d7ca5907adf58ec9d4ee5765086787e637
jasb <jason.becker@parcIT.de>
2024-04-07 13:30:02 +0200

    Settings for DocumentRootPath

---
+++ backend/settings/st-adapter/st-spring-autoconfigure/src/main/java/com/jabasoft/mms/settings/config/SettingsSpringAutoconfigure.java
@@ -0,0 +1,10 @@
+package com.jabasoft.mms.settings.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.jabasoft.mms.settings")
+class SettingsSpringAutoconfigure {
+
+}
