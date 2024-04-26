417acae2b6e8a70177b416a9b39d4f70036bd92f
jasb <jason.becker@parcIT.de>
2024-04-26 08:00:19 +0200

    Autocomplete Testing

--- angular-playground/src/app/app.component.ts
+++ angular-playground/src/app/app.component.ts
@@ -1,14 +1,51 @@
-import { Component } from '@angular/core';
+import {AsyncPipe, NgForOf} from "@angular/common";
+import {Component, forwardRef, OnInit} from '@angular/core';
+import {FormControl, NG_VALUE_ACCESSOR, ReactiveFormsModule} from "@angular/forms";
+import {MatAutocomplete, MatAutocompleteTrigger, MatOption} from "@angular/material/autocomplete";
+import {MatFormField} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
 import { RouterOutlet } from '@angular/router';
+import {map, Observable, startWith} from "rxjs";
 import {AutocompleteComponent} from "./autocomplete/autocomplete.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AutocompleteComponent],
+  imports: [RouterOutlet, AutocompleteComponent, MatFormField, MatInput, MatAutocomplete, MatOption, NgForOf, MatAutocompleteTrigger, ReactiveFormsModule, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'frontend-playground';
+
+  options: string[] = ['Angular', 'React', 'Vue'];
+
+  objectOptions = [
+    {name: 'Angular'},
+    {name: 'Angular Material'},
+    {name: 'React'},
+    {name: 'Vue'}
+  ];
+
+  myControl = new FormControl();
+
+  filteredOptions: Observable<string[]> = this.myControl.valueChanges;
+
+  ngOnInit(): void {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this.filter(value))
+    )
+  }
+
+
+  //Wird benutzt, um im Input Feld nicht [object Object] angezeigt zu bekommen, sondern einen schönen Namen
+  displayFun(subject: { name: any; }){
+    return subject ? subject.name : undefined;
+  }
+
+  private filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
 }
--- angular-playground/src/app/autocomplete/autocomplete.component.ts
+++ angular-playground/src/app/autocomplete/autocomplete.component.ts
@@ -1,80 +1,47 @@
-import {AsyncPipe} from "@angular/common";
+import {NgForOf} from "@angular/common";
 import { Component } from '@angular/core';
 import {FormControl, ReactiveFormsModule} from "@angular/forms";
 import {MatAutocomplete, MatAutocompleteTrigger, MatOption} from "@angular/material/autocomplete";
 import {MatFormField} from "@angular/material/form-field";
-import {MatSlideToggle} from "@angular/material/slide-toggle";
-import {map, Observable, startWith} from "rxjs";
-
-export class State {
-  constructor(public name: string, public population: string, public flag: string) { }
-}
+import {MatInput} from "@angular/material/input";
 
 @Component({
   selector: 'app-autocomplete',
   standalone: true,
   imports: [
     MatFormField,
+    ReactiveFormsModule,
+    MatAutocompleteTrigger,
     MatAutocomplete,
     MatOption,
-    MatSlideToggle,
-    MatFormField,
-    MatAutocompleteTrigger,
-    ReactiveFormsModule,
-    AsyncPipe
+    NgForOf,
+    MatInput
+
   ],
   templateUrl: './autocomplete.component.html',
   styleUrl: './autocomplete.component.css'
 })
 export class AutocompleteComponent {
+  myControl = new FormControl();
+  states: { value: string; display: string; }[] = [];
 
-  stateCtrl: FormControl;
-  filteredStates: Observable<any[]>;
-
-  states: State[] = [
-    {
-      name: 'Arkansas',
-      population: '2.978M',
-      // https://commons.wikimedia.org/wiki/File:Flag_of_Arkansas.svg
-      flag: 'https://upload.wikimedia.org/wikipedia/commons/9/9d/Flag_of_Arkansas.svg'
-    },
-    {
-      name: 'California',
-      population: '39.14M',
-      // https://commons.wikimedia.org/wiki/File:Flag_of_California.svg
-      flag: 'https://upload.wikimedia.org/wikipedia/commons/0/01/Flag_of_California.svg'
-    },
-    {
-      name: 'Florida',
-      population: '20.27M',
-      // https://commons.wikimedia.org/wiki/File:Flag_of_Florida.svg
-      flag: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Florida.svg'
-    },
-    {
-      name: 'Texas',
-      population: '27.47M',
-      // https://commons.wikimedia.org/wiki/File:Flag_of_Texas.svg
-      flag: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Texas.svg'
-    }
-  ];
-
-  constructor() {
-    this.stateCtrl = new FormControl();
-    this.filteredStates = this.stateCtrl.valueChanges
-      .pipe(
-        startWith(''),
-        map(state => state ? this.filterStates(state) : this.states.slice())
-      );
+  constructor(){
+    this.loadStates();
   }
-
-  filterStates(name: string) {
-    return this.states.filter(state =>
-      state.name.toLowerCase().indexOf(name.toLowerCase()) === 0);
-  }
-
-  onEnter(evt: any){
-    if (evt.source.selected) {
-      alert("hello ");
-    }
+  //build list of states as map of key-value pairs
+  loadStates() {
+    var allStates = 'Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware,\
+         Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana,\
+         Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana,\
+         Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina,\
+         North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, South Carolina,\
+         South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia,\
+         Wisconsin, Wyoming';
+    this.states =  allStates.split(/, +/g).map( function (state) {
+      return {
+        value: state.toUpperCase(),
+        display: state
+      };
+    });
   }
 }
