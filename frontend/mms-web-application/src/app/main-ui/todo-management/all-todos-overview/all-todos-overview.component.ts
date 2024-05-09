b7f435b24938069c2c254bb7aaad13589bc67293
jasb <jason.becker@parcIT.de>
2024-05-09 09:56:44 +0200

    Sorting Todos works

--- frontend/mms-web-application/src/app/main-ui/todo-management/all-todos-overview/all-todos-overview.component.ts
+++ frontend/mms-web-application/src/app/main-ui/todo-management/all-todos-overview/all-todos-overview.component.ts
@@ -28,32 +28,30 @@ export class AllTodosOverviewComponent {
 
   constructor(todoManagementService: TodoService) {
     todoManagementService.getAllTodos((recTodos: Todo[]) => {
-      let uiId = 1;
 
       for (let todo of recTodos) {
-        this.todos.push(new TodoManagementTodo(uiId, todo));
-        uiId++;
+        this.todos.push(new TodoManagementTodo(1, todo));
       }
+
+      this.sortTodos();
     })
   }
 
   public onTodoDeleted(deleted: TodoManagementTodo) {
     this.currentlySelectedUiId = -1;
     this.todos.splice(deleted.uiId - 1, 1);
-    let uiId = 1;
-    for (let todo of this.todos) {
-      todo.uiId = uiId;
-      uiId++;
-    }
+    this.sortTodos();
   }
 
   public onTodoEdited(edited: TodoManagementTodo) {
     this.todos[edited.uiId - 1] = edited;
+    this.sortTodos();
   }
 
   public onTodoAdded(added: Todo) {
     let todoManagementTodo = new TodoManagementTodo(this.todos.length + 1, added);
     this.todos.push(todoManagementTodo);
+    this.sortTodos();
   }
 
   onTodoClicked(todo: TodoManagementTodo) {
@@ -82,5 +80,40 @@ export class AllTodosOverviewComponent {
       styles.push('all-todo-overview-card-closed')
     }
     return styles;
+  }
+
+  rearrangeTodos(){
+    let uiId = 1;
+    for (let todo of this.todos) {
+      todo.uiId = uiId;
+      uiId++;
+    }
+  }
+
+  sortTodos(){
+    let closedTodos = this.todos.filter(todo => todo.todo.isClosed());
+    let unclosedTodosWithEndTime = this.todos.filter(todo => !todo.todo.isClosed() && todo.todo.endTime);
+    let unclosedTodosWithoutEndTime = this.todos.filter(todo => !todo.todo.isClosed() && !todo.todo.endTime);
+
+    closedTodos = closedTodos.sort((a, b) => {
+      if(a.todo.closedTime && b.todo.closedTime) {
+        return b.todo.closedTime.getTime() - a.todo.closedTime.getTime();
+      }
+      return b.todo.creationTime.getTime() - a.todo.creationTime.getTime();
+    })
+
+    unclosedTodosWithEndTime = unclosedTodosWithEndTime.sort((a, b) => {
+      if(a.todo.endTime && b.todo.endTime) {
+        return a.todo.endTime.getTime() - b.todo.endTime.getTime();
+      }
+      return a.todo.creationTime.getTime() - b.todo.creationTime.getTime();
+    })
+
+    unclosedTodosWithoutEndTime = unclosedTodosWithoutEndTime.sort((a, b) => {
+      return a.todo.creationTime.getTime() - b.todo.creationTime.getTime();
+    })
+
+    this.todos = unclosedTodosWithEndTime.concat(unclosedTodosWithoutEndTime).concat(closedTodos);
+    this.rearrangeTodos();
   }
 }
